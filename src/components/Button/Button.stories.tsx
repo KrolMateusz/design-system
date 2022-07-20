a74fb065ae2cfe9a90a8b5617766404ae7d12f61
Mateusz Król <mateusz.krol@boldare.com>
2022-07-20 09:02:24 +0200

    button component

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,22 +1,30 @@
 import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
-import Button, { ButtonProps } from "./Button";
+import Button from "./Button";
 
 export default {
   title: "Components/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    color: {
+      control: 'select',
+      options: ['yellow', 'blue', 'violet', 'red', 'green', 'white', 'primary']
+    },
+    variant: {
+      control: 'select',
+      options: ['contained', 'outlined' ]
+    },
+    size: {
+      control: 'select',
+      options: ['large', 'medium', 'small' ]
+    },
+  }
 } as Meta;
 
 // Create a master template for mapping args to render the Button component
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story = (args) => <Button {...args}>Button</Button>;
 
 // Reuse that template for creating different stories
 export const Primary = Template.bind({});
-Primary.args = { label: "Primary 😃", size: "large" };
-
-export const Secondary = Template.bind({});
-Secondary.args = { ...Primary.args, primary: false, label: "Secondary 😇" };
+Primary.args = { variant: 'contained', color: 'primary', size: 'large', disabled: true };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,55 +1,79 @@
 import React from "react";
-import "./Button.css";
+import ButtonBase from '@mui/material/ButtonBase';
+import { styled, Theme } from '@mui/system';
 
-export interface ButtonProps  {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => void;
-};
+const getBorderColor = (variant: string, color: string, theme: Theme) => {
+  if (variant === 'contained') {
+    if (color === 'primary') {
+      return theme.palette.white.main
+    }
+    return theme.palette.primary.main
+  }
+  return theme.palette[ color ].main
+}
 
-/**
- * Primary UI component for user interaction
- */
-const Button = ({
-  primary = true,
-  backgroundColor,
-  size = "medium",
-  onClick,
-  label,
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+const getShadow = (theme: Theme, variant: string) => {
+  if (variant === 'contained') {
+    return {
+      content: '""',
+      position: 'absolute',
+      transition: 'transform 0.6s cubic-bezier(0.2, 1, 0.25, 1)',
+      zIndex: -1,
+      top: -2,
+      right: -2,
+      bottom: -2,
+      left: -2,
+      boxShadow: theme.elevation.primary.main,
+    }
+  }
+  return {
+    content: '""',
+    position: 'absolute',
+    transition: 'transform 0.6s cubic-bezier(0.2, 1, 0.25, 1)',
+    zIndex: -1,
+    top: 4,
+    left: -8,
+    bottom: -4,
+    width: '100%',
+    height: '100%',
+    backgroundImage: `linear-gradient(${theme.palette[ color! ].main}, ${theme.palette[ color! ].main}), linear-gradient(to right, ${theme.palette[ color! ].main}, ${theme.palette[ color! ].main}), linear-gradient(to bottom, ${theme.palette[ color! ].main}, ${theme.palette[ color! ].main}), linear-gradient(to left, ${theme.palette[ color! ].main}, ${theme.palette[ color! ].main}), linear-gradient(transparent, transparent)`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '2px 100%, 2px 100%, 2px 100%, 2px 100%, calc(100% - 4px) 100%',
+    backgroundPosition: 'left bottom, left bottom, right top, right top, 4px 4px'
+  }
+}
+
+const StyledButton = styled(ButtonBase)<{ color?: string, size?: string, variant?: string }>(({ theme, color, size, variant }) => ({
+  backgroundColor: variant === 'contained' ? theme.palette[ color! ].main : 'transparent',
+  padding: size === 'large' ? `${theme.spacing(2, 3)}` : size === 'medium' ? `${theme.spacing(1, 2)}` : `4px 10px`,
+  fontWeight: 700,
+  textTransform: 'uppercase',
+  color: variant === 'contained' ? theme.palette[ color! ].contrastText : theme.palette[ color! ].main,
+  border: `2px solid ${getBorderColor(variant!, color!, theme)}`,
+  position: 'relative',
+  transition: 'transform 0.6s cubic-bezier(0.2, 1, 0.25, 1)',
+  willChange: 'transform',
+  '&::before': getShadow(theme, variant!),
+  '&:hover': {
+    transform: 'translateX(-4px) translateY(4px)',
+  },
+  '&:hover::before': {
+    transform: 'translateX(2px) translateY(-2px)',
+  },
+  '&:disabled': {
+    backgroundColor: theme.palette.action.disabledBackground,
+    border: `2px solid ${theme.palette.action.disabledBackground}`,
+    color: theme.palette.action.disabled
+  },
+  '&:disabled::before': {
+    boxShadow: theme.elevation.disabled.main
+  }
+}));
+
+const Button = ({ children, ...props }) => {
+  
   return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={backgroundColor ? { backgroundColor }: {}}
-      onClick={onClick}
-    >
-      {label}
-    </button>
+    <StyledButton {...props}>{children}</StyledButton>
   );
 };
 
