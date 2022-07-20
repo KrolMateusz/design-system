ccf25ee096a949055424c7d9b7fa63c93f5a4327
Mateusz Król <mateusz.krol@boldare.com>
2022-07-20 09:03:21 +0200

    text field

---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+import TextField, { TextFieldProps } from "./TextField";
+
+export default {
+    title: 'Components/TextField',
+    component: TextField,
+} as Meta
+
+const Template: Story<TextFieldProps> = (args: TextFieldProps) => <TextField {...args} />
+
+export const PrimaryTextField = Template.bind({});
+PrimaryTextField.args = {
+    label: 'Hello',
+}
---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MuiTextField, { TextFieldProps as MuiTextFieldProps } from '@mui/material/TextField';
+
+export type TextFieldProps = MuiTextFieldProps
+
+const TextField = (props: TextFieldProps) => {
+  return (
+    <MuiTextField {...props} />
+  )
+}
+
+export default TextField
