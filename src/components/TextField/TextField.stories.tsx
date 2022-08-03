127218da313d4db9263a241fe59aed90a3aabf42
Mateusz Król <37661247+KrolMateusz@users.noreply.github.com>
2022-08-03 14:30:17 +0200

    Text field props

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Story, Meta } from '@storybook/react'
 import TextField, { TextFieldProps } from './TextField'
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircleSharp from '@mui/icons-material/AccountCircleSharp'
 
 export default {
   title: 'Components/TextField',
@@ -9,7 +11,26 @@ export default {
 
 const Template: Story<TextFieldProps> = (args: TextFieldProps) => <TextField {...args} />
 
-export const PrimaryTextField = Template.bind({})
-PrimaryTextField.args = {
+export const Standard = Template.bind({})
+Standard.args = {
   label: 'Hello',
+}
+
+export const Error = Template.bind({})
+Error.args = {
+  label: 'Hello',
+  error: true,
+  helperText: 'Some error'
+}
+
+export const StartIcon = Template.bind({})
+StartIcon.args = {
+  label: 'Input with icon',
+  startAdornment: (<InputAdornment position='start'><AccountCircleSharp /></InputAdornment>),
+}
+
+export const EndIcon = Template.bind({})
+EndIcon.args = {
+  label: 'Input with icon',
+  endAdornment: (<InputAdornment position='end'><AccountCircleSharp /></InputAdornment>),
 }
--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import MuiTextField, { TextFieldProps as MuiTextFieldProps } from '@mui/material/TextField'
+import React, { useEffect, useState } from 'react'
+import MuiTextField from '@mui/material/TextField'
 
-export type TextFieldProps = MuiTextFieldProps
+export interface TextFieldProps {
+  label: string
+  size?: 'small' | 'medium'
+  error?: boolean
+  helperText?: string
+  multiline?: boolean
+  maxRows?: number
+  startAdornment?: React.ReactNode
+  endAdornment?: React.ReactNode
+  onChange: (value: string) => void
+}
 
-const TextField = (props: TextFieldProps) => {
-  return <MuiTextField {...props} />
+const TextField = ({ startAdornment, endAdornment, onChange, ...props }: TextFieldProps) => {
+  const [value, setValue] = useState<string | undefined>()
+
+  useEffect(() => {
+    onChange(value)
+  }, [ value ])
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
+
+  return <MuiTextField InputProps={{ startAdornment, endAdornment }} value={value} onChange={handleChange} {...props} />
 }
 
 export default TextField
